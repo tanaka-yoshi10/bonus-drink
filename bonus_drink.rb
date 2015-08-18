5a9fc0a6022df8ed0f364fafe2c49aeb57fb97fa
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-08-19 08:02:48 +0900

    first version

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,12 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    nokori = amount
+    sum = 0
+    while nokori >= 3
+      nokori -= 3
+      sum += 3
+      nokori += 1
+    end
+    return sum + nokori
   end
 end
